79c01d935ee8777ee22adce064a60a4749048f52
Justyna Siedlarz <tysia515@gmail.com>
2018-10-23 21:12:35 +0200

    lab4-4

--- complex.js
+++ complex.js
@@ -1,54 +1,46 @@
-var ComplexNumber = /** @class */ (function () {
-    function ComplexNumber(real, imaginary) {
+"use strict";
+class ComplexNumber {
+    constructor(real, imaginary) {
         this._imaginary = imaginary;
         this._real = real;
     }
-    Object.defineProperty(ComplexNumber.prototype, "Real", {
-        get: function () {
-            return this._real;
-        },
-        set: function (value) {
-            this._real = value;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(ComplexNumber.prototype, "Imaginary", {
-        get: function () {
-            return this._imaginary;
-        },
-        set: function (value) {
-            this._imaginary = value;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    ComplexNumber.prototype.add = function (digit2) {
-        var result;
+    get Real() {
+        return this._real;
+    }
+    set Real(value) {
+        this._real = value;
+    }
+    get Imaginary() {
+        return this._imaginary;
+    }
+    set Imaginary(value) {
+        this._imaginary = value;
+    }
+    add(digit2) {
+        let result;
         this._real += digit2.Real;
         this._imaginary += digit2.Imaginary;
         result = this._real + " " + this._imaginary + "j";
         return this._real + " " + this._imaginary + "j + " + digit2.Real + " " + digit2.Imaginary + "j = " + result;
-    };
-    ComplexNumber.prototype.subtract = function (digit2) {
-        var result;
+    }
+    subtract(digit2) {
+        let result;
         this._real -= digit2.Real;
         this._imaginary -= digit2.Imaginary;
         result = this._real + " " + this._imaginary + "j";
         return this._real + " " + this._imaginary + "j - " + digit2.Real + " " + digit2.Imaginary + "j = " + result;
-    };
-    ComplexNumber.prototype.module = function () {
-        var result;
+    }
+    module() {
+        let result;
         result = Math.sqrt(this._real * this._real + this._imaginary * this._imaginary);
         return "|" + this._real + " " + this._imaginary + "j| = " + result;
-    };
-    ComplexNumber.prototype.show = function () {
+    }
+    show() {
         return this._real + " " + this._imaginary + "j";
-    };
-    return ComplexNumber;
-}());
-var digit1 = new ComplexNumber(5, 9);
-var digit2 = new ComplexNumber(1, 3);
+    }
+}
+let digit1 = new ComplexNumber(5, 9);
+let digit2 = new ComplexNumber(1, 3);
 console.log(digit1.add(digit2));
 console.log(digit1.subtract(digit2));
 console.log(digit1.module());
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 var result = ' x   ';
 function buff(val) {
     var buff = '';
